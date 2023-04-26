fda8222db908278de5f6b072c6ce8e3e52a1f4ad
Donald Abua <abuadonald@gmail.com>
2023-04-26 13:52:13 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void directories(void);
+char **_strtok(char *string, char delimiter);
+#endif /*MAIN_H*/
