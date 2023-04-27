f1860ec0e8fada5d9904d39736a50a62416141ca
Donald Abua <abuadonald@gmail.com>
2023-04-26 22:02:45 -0700

    betty_store.c

--- store.c
+++ store.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ *store - stores split strings in an array
+ *@string: string being split
+ *@start: copy start
+ *@end: copy end
+ *@array: array to store split string
+ *Return: void
+ */
 void store(char *string, int start, int end, char *array)
 {
 int i, j = 0;
