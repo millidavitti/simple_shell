ef221c956a0724730dfe6e47f689c43d2c97ea13
Donald Abua <abuadonald@gmail.com>
2023-04-26 22:06:02 -0700

    betty__strtok.c

--- _strtok.c
+++ _strtok.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ *_strtok - splits string by its delimiter
+ *@string: string to split
+ *@delimiter: delimiter
+ *Return: array of strings
+ */
 char **_strtok(char *string, char delimiter)
 {
 int i, start = 0, j = 0;
@@ -21,16 +26,14 @@ return (NULL);
 
 for (i = 0; string[i]; i++)
 {
- 
- if (string[i] == delimiter)
- {
- store(string, start, i, str_arr[j]);
- start = i + 1;
- j++;
- }
- else
- store(string, start, i, str_arr[j]);
+if (string[i] == delimiter)
+{
+store(string, start, i, str_arr[j]);
+start = i + 1;
+j++;
 }
-
+else
+store(string, start, i, str_arr[j]);
+}
 return (str_arr);
 }
